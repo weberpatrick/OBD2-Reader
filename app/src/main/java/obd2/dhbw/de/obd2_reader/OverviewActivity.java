b343f1bb573807e1b9afabca231e7046928aefce
Patrick Weber <patrick.weber1@outlook.com>
2016-04-06 15:29:51 +0200

    - added OverviewActivity with Layout-file

---
+++ app/src/main/java/obd2/dhbw/de/obd2_reader/OverviewActivity.java
@@ -0,0 +1,13 @@
+package obd2.dhbw.de.obd2_reader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+    }
+}
