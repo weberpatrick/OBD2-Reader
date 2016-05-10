2094789eb3a5cf0be06c2eb41c336913dd5041f8
Patrick Weber <patrick.weber1@outlook.com>
2016-05-10 15:56:42 +0200

    just show the Name in the drawer, the id is invisible delete trips from the drawer rename trips from the drawer

--- app/src/main/java/obd2/dhbw/de/obd2_reader/MainActivity.java
+++ app/src/main/java/obd2/dhbw/de/obd2_reader/MainActivity.java
@@ -16,18 +16,25 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.util.Log;
 import android.util.Pair;
+import android.view.ContextMenu;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -269,6 +276,7 @@ public class MainActivity
         textViewThrottlePositionValue   = (TextView) findViewById(R.id.textViewThrottlePositionValue);
 
         drawerList = (ListView)findViewById(R.id.drawerList);
+        registerForContextMenu(drawerList);
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         title = getTitle().toString();
         addDrawerItems();
@@ -284,7 +292,8 @@ public class MainActivity
         if (tripIdArray.length>0)
         {
             for (int i : tripIdArray){
-                tripStringArray.add(new String[] {"Trip ", String.valueOf(i), "(" + dbHelper.selectTrip(i).getDate() + ")"});
+                TripRow row = dbHelper.selectTrip(i);
+                tripStringArray.add(new String[] {"Trip"/*row.getName*/, String.valueOf(i), "(" + row.getDate() + ")"});
             }
         }else{
             // No trips found
@@ -301,13 +310,13 @@ public class MainActivity
                 View view = inflater.inflate(R.layout.drawer_row, parent, false);
 
                 // 3. Get the textViews from the view
-                TextView tripText = (TextView) view.findViewById(R.id.drawerRowTrip);
+                TextView tripName = (TextView) view.findViewById(R.id.drawerRowName);
                 TextView tripId   = (TextView) view.findViewById(R.id.drawerRowId);
                 TextView tripDate = (TextView) view.findViewById(R.id.drawerRowDate);
 
                 // 4. Set the text for textViews
                 String[] row = tripStringArray.get(position);
-                tripText.setText(row[0]);
+                tripName.setText(row[0]);
                 tripId.setText(row[1]);
                 tripDate.setText(row[2]);
 
@@ -322,10 +331,12 @@ public class MainActivity
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 ViewGroup group = (ViewGroup) view;
-                TextView tripId = (TextView) group.findViewById(R.id.drawerRowId);
+                TextView tripName = (TextView) group.findViewById(R.id.drawerRowName);
+                TextView tripId   = (TextView) group.findViewById(R.id.drawerRowId);
 
+                //do not show the TripMessage, if the user clicks the "no trips found" text
+                if (!tripName.getText().equals(getString(R.string.noTripsFound)))
                 showTripMessage(Integer.parseInt(tripId.getText().toString()));
-
             }
         });
 
@@ -367,17 +378,11 @@ public class MainActivity
     */
     private void refreshDrawer()
     {
-        String date = "";
-        TripRow row = dbHelper.selectTrip(currentTripId);
-
-        //If the device has no Bluetooth, and the user hits the start Button,
-        //this here is going to be called and would throw an Exception
-        if (row != null)
-        date = row.getDate();
-
-        //add "Trip ", id, date to the top of the List and update the listAdapter
-        tripStringArray.add(0, new String[] {"Trip ", String.valueOf(currentTripId),"(" +  date + ")"});
-
+        if (tripStringArray.isEmpty()){
+            tripStringArray.add(new String[] {getString(R.string.noTripsFound), "", ""});
+        }else{
+            tripStringArray.remove(new String[] {getString(R.string.noTripsFound), "", ""});
+        }
         new Handler(Looper.getMainLooper()).post(new Runnable()
         {
             @Override
@@ -398,6 +403,162 @@ public class MainActivity
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * create Context Menu
+     */
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo){
+        ViewGroup group = (ViewGroup) view;
+        TextView tripName = (TextView) group.findViewById(R.id.drawerRowName);
+
+        //do not show the TripMenu, if the user holds "No trips found" text
+        if (!tripName.getText().equals(getString(R.string.noTripsFound))){
+            super.onCreateContextMenu(menu, view, menuInfo);
+
+            if (view.getId() == R.id.drawerList) {
+                MenuInflater inflater = getMenuInflater();
+                inflater.inflate(R.menu.trip_menu, menu);
+            }
+        }
+    }
+
+    /**
+     * implements the behavior of the trip context menu
+     */
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int pos = info.position;
+        int id = Integer.parseInt(drawerAdapter.getItem(pos)[1]);
+
+        switch (item.getItemId()){
+            case R.id.tripShow:
+                showTripMessage(id);
+                return true;
+            case R.id.tripRename:
+                showRenameTripDialog(id);
+                return true;
+            case R.id.tripDelete:
+                showDeleteTripDialog(id);
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    /**
+     * shows the dialog to enter a new tripName
+     */
+    private void showRenameTripDialog(final int id) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle(R.string.newTripNameTitle);
+
+        //Input EditText
+        final EditText input = new EditText(this);
+        //Text input. Capitalizes the first letter after a ". ", or at the beginning of the text
+        input.setInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+        //Max Length is 15 chars
+        input.setFilters(new InputFilter[]{
+                new InputFilter.LengthFilter(15)
+        });
+
+        //put EditText into Dialog
+        builder.setView(input);
+        //OK Button: renaming the Trip
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                renameTrip(id, input.getText().toString());
+            }
+        });
+        //Cancel Button: close the dialog and do nothing
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                dialog.cancel();
+            }
+        });
+
+        final AlertDialog dialog = builder.create();
+        //Open keyboard
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+
+        dialog.show();
+        //Listener, if the User clicks on ENTER on the keyboard, it will also rename the trip
+        // and closes the dialog
+        input.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == event.KEYCODE_ENTER){
+                    renameTrip(id, input.getText().toString());
+                    dialog.cancel();
+                }
+                return false;
+            }
+        });
+    }
+
+    /**
+     * updates the trip in the database and drawer
+     */
+    private void renameTrip(int id, String newTripName) {
+
+        //Just update the database and drawer, if the user enters a NEW name and NOT an empty String
+//        if (!newTripName.equals(dbHelper.selectTrip(id).getName) && !newTripName.isEmpty()) {
+//            dbHelper.updateTripName(id, newTripName);
+        for (String[] row : tripStringArray) {
+            if (Integer.parseInt(row[1]) == id) {
+                row[0] = newTripName;
+            }
+        }
+
+        refreshDrawer();
+//        }
+    }
+
+    /**
+     * deletes the trip from the database and drawer
+     */
+    private void showDeleteTripDialog(final int id) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert!!");
+        builder.setMessage("Are you sure to delete this trip?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                deleteTrip(id);
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
+
+    /**
+     * deletes the trip from the database and drawer
+     */
+    private void deleteTrip(int id) {
+//        dbHelper.deleteTrip(id);
+        String [] rowToDelete = new String[3];
+
+        for (String[] row : tripStringArray){
+            if (Integer.parseInt(row[1]) == id){
+                rowToDelete = row;
+            }
+        }
+        tripStringArray.remove(rowToDelete);
+        refreshDrawer();
     }
 
     /**
@@ -635,6 +796,17 @@ public class MainActivity
     private void endTrip()
     {
         if(dbHelper != null) TripCalculator.calculate(dbHelper, currentTripId, INPUT_DATA_INTERVAL);
+
+        String date = "";
+        TripRow row = dbHelper.selectTrip(currentTripId);
+
+        //If the device has no Bluetooth, and the user hits the start Button,
+        //this here is going to be called and would throw an Exception
+        if (row != null)
+            date = row.getDate();
+
+        //add "Trip ", id, date to the top of the List and update the listAdapter
+        tripStringArray.add(0, new String[] {"Trip"/*row.getName*/, String.valueOf(currentTripId),"(" +  date + ")"});
 
         refreshDrawer();
 
